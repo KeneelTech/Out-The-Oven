d9adb232b0a53b2e40813b605f0db54774c5bd48
Keneel <tkeneel@gmail.com>
2024-11-22 20:53:01 -0500

    Set up of database connection

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.beans.Statement;
+import java.sql.*;
+
+
 public class Main {
+    static final String db_url = "jdbc:mysql://127.0.0.1:3306/out_the_oven";
+    static final String user = "root";
+    static final String pass = "";
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        try(Connection conn = DriverManager.getConnection(db_url, user, pass);
+            java.sql.Statement stmt = conn.createStatement();
+        ){
+            
+        } catch (SQLException e){
+            e.printStackTrace();
         }
+            
     }
 }
