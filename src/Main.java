3715ac4816fd527a6571ee6808f1b64f3916bd97
Keneel <tkeneel@gmail.com>
2024-12-01 16:15:28 -0500

    Minor bug fix

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
-import workingdaycalendar.WorkingDayCalendarUI;
-
 public class Main {
 
     public static void main(String[] args) {
-        WorkingDayCalendarUI calendar = new WorkingDayCalendarUI();
-        calendar.displayUI();
+        Welcomescreen.main(args);
 
     }
 }
